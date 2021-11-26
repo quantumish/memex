4889c556cabb16bf4fdfb81ff5a0598a7abc5f48
David Freifeld <freifeld.david@gmail.com>
2021-11-26 00:40:48 -0800

    Code compiles

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,9 +1,7 @@
 pub mod requests;
 pub use crate::requests::*;
 
-#[cfg(feature = "toggl")]
 pub mod toggl;
-#[cfg(feature = "toggl")]
 pub use crate::toggl::*;
 
 use chrono::{Duration, DateTime, Local};
@@ -16,22 +14,15 @@ use std::fs::{OpenOptions};
 use serde::{Serialize, Deserialize};
 use config::*;
 use std::env;
+use anyhow::{Result, Error};
 
 const MAX_CACHE_LEN: u32 = 1;
 const LOG_FMT_STRING: &'static str = "\\* [`%i`] %S-%E *%n* (%p)\n";
 const DISPLAY_FMT_STRING: &'static str =
     "[`%i`] *%n*\n**Start**: %s\n**Stop**: %e\n**Tags**: %t\n**Project**: %p\n";
-const ALPHABET: [char; 16] = [
+const HASH_ALPHABET: [char; 16] = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'
 ];
-
-fn unpack(mut s: Vec<u8>) -> String {
-    s.retain(|&x| x != 0);
-    match std::str::from_utf8(&s) {
-	Ok(x) => String::from(x),
-	Err(_x) => panic!("Failed to unpack."),
-    }
-}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 struct Tag {
@@ -73,7 +64,7 @@ impl Block {
 	    end: None,
 	    tags: Vec::new(),
 	    project: None,
-	    id: nanoid!(8, &ALPHABET),
+	    id: nanoid!(8, &HASH_ALPHABET),
 	}
     }
 
@@ -119,6 +110,10 @@ struct Handler {
     current: Option<Block>,
 }
 
+struct HandlerWrapper {h: *mut Handler}
+unsafe impl Send for HandlerWrapper {}
+unsafe impl Sync for HandlerWrapper {}
+
 impl Handler {
     fn new() -> Handler {
 	Handler {
@@ -151,7 +146,7 @@ impl Handler {
 	}
     }
 
-    fn stop_current(&mut self) {
+    fn stop_current(&mut self) -> Result<(), Error> {
 	if let Some(cur) = &mut self.current {
 	    cur.stop();
 	    self.cache.push(cur.clone());
@@ -163,60 +158,56 @@ impl Handler {
 		    OpenOptions::new().read(true).open(&self.file).unwrap());
 		writer.write((serde_json::to_string(&self.cache[0]).unwrap()+"\n").as_bytes()).unwrap();
 		for line in reader.lines() {
-		    writer.write((line.unwrap()+"\n").as_bytes());
+		    writer.write((line.unwrap()+"\n").as_bytes())?;
 		}
-		std::fs::rename("data.json.tmp", "data.json");
+		std::fs::rename("data.json.tmp", "data.json")?;
 		self.cache.drain(0..1);
 	    }
 	}
-    }
-
-    async fn add_new(&mut self, name: String, proj: String) -> std::result::Result<(), &'static str> {
-	self.stop_current();
-	let mut tmp: Block = Block::new();
-	tmp.name = name;
-	tmp.project = Some(Project {name: proj}); // Questionable
-	if cfg!(feature = "toggl") {
-	    let token = self.settings.get::<String>("toggl.api_token").unwrap();
-	    let map = get_projects(token.clone(), self.settings.get::<u64>("toggl.default_workspace").unwrap()).await;
-	    let pid: u64;
-	    match tmp.project {
-		Some(ref p) => pid = get_proj_id(token.clone(), map, p.name.clone()).await,
-		None => pid = 0, 
-	    }
-	    set_toggl(token.clone(), tmp.name.clone(), pid).await;			
-	}
-	self.current = Some(tmp);		
 	Ok(())
     }
 
-    async fn handle_add(&mut self, stream: &TcpStream, e: Entity) {		
+    async fn add_new(&mut self, name: String, proj: String) -> Result<(), Error> {
+	self.stop_current()?;
+	let mut tmp: Block = Block::new();
+	tmp.name = name;
+	tmp.project = Some(Project {name: proj}); // Questionable	
+	let token = self.settings.get::<String>("toggl.api_token").unwrap();
+	let map = get_projects(token.clone(), self.settings.get::<u64>("toggl.default_workspace").unwrap()).await;
+	let pid: u64;
+	match tmp.project {
+	    Some(ref p) => pid = get_proj_id(token.clone(), map, p.name.clone()).await,
+	    None => pid = 0,
+	}
+	set_toggl(token.clone(), tmp.name.clone(), pid).await;	
+	self.current = Some(tmp);
+	Ok(())
+    }
+    
+    async fn handle_add(&mut self, e: Entity) -> Result<(), Error> {
 	match e {
-	    Entity::Block(name, proj) => 
-		self.add_new(unpack(name.to_vec()), unpack(proj.to_vec())).await.unwrap(),							
+	    Entity::Block(name, proj) => {self.add_new(name, proj).await?;},
 	    Entity::Tag(tag) => {
 		if let Some(i) = &mut self.current {
-		    i.tags.push(Tag {name: unpack(tag.to_vec())});
-		    if cfg!(feature = "toggl") {
-			let token = self.settings.get::<String>("toggl.api_token").unwrap(); 
-			// HACK Make it so args are optional in update_toggl
-			let map = get_projects(token.clone(), self.settings.get::<u64>("toggl.default_workspace").unwrap()).await;
-			let pid: u64;
-			match &i.project {							
-			    Some(p) => pid = get_proj_id(token.clone(), map, p.name.clone()).await,
-			    None => pid = 0, 
-			}
-			update_toggl(token.clone(), i.name.clone(), i.tags.iter().map(|x| x.name.clone()).collect(), pid).await;
+		    i.tags.push(Tag {name: tag});
+		    let token = self.settings.get::<String>("toggl.api_token").unwrap();
+		    // HACK Make it so args are optional in update_toggl
+		    let map = get_projects(token.clone(), self.settings.get::<u64>("toggl.default_workspace").unwrap()).await;
+		    let pid: u64;
+		    match &i.project {
+			Some(p) => pid = get_proj_id(token.clone(), map, p.name.clone()).await,
+			None => pid = 0,
 		    }
-		} else {write_stream(stream, String::from("ERR: no existing block"));}
-
+		    update_toggl(token.clone(), i.name.clone(), i.tags.iter().map(|x| x.name.clone()).collect(), pid).await;
+		}
 	    }
 	    Entity::Project(proj) => {
-		if let Some(i) = &mut self.current {
-		    i.project = Some(Project {name: unpack(proj.to_vec())});
-		} else {write_stream(stream, String::from("ERR: no existing block"));}
+		if let Some(c) = &mut self.current {
+		    c.project = Some(Project {name: proj});
+		}
 	    }
 	}
+	Ok(())
     }
 
     fn handle_get(&self, stream: &TcpStream, s: Specifier) {
@@ -232,18 +223,16 @@ impl Handler {
 		}
 	    },
 	    Specifier::Id(id) => {
-		let ident = unpack(id.to_vec());
 		for block in self.iter() {
-		    if block.id.eq(&ident) {
+		    if block.id.eq(&id) {
 			write_stream(stream, block.to_format(format.clone()));
 		    }
 		}
-		write_stream(stream, String::from("ERR: invalid ID"));
 	    }
 	}
     }
 
-    fn handle_log(&self, mut stream: &TcpStream, r: Range) { // TODO use fmt
+    fn handle_log(&self, stream: &TcpStream, r: Range) { // TODO use fmt
 	match r {
 	    Range::Term(t) => match t {
 		Term::All => {
@@ -262,76 +251,72 @@ impl Handler {
 		_ => (),
 	    },
 	    _ => (),
-	}		
+	}
     }
 
     fn iter(&self) -> std::iter::Chain<impl Iterator<Item = Block> + '_, impl Iterator<Item = Block>> {
-	let mut reader = std::io::BufReader::new(
-	    OpenOptions::new().read(true).open(&self.file).unwrap());
-	self.cache.iter().cloned().
-	    chain(reader.lines()
-		  .map(|line| serde_json::from_str(&line.unwrap().trim()).unwrap()))
+	let reader = std::io::BufReader::new(OpenOptions::new().read(true).open(&self.file).unwrap());
+	self.cache.iter().cloned().chain(reader.lines().map(
+	    |line| serde_json::from_str(&line.unwrap().trim()).unwrap()))
     }
 }
 
+unsafe fn spawn_thread(handler_wrapper: HandlerWrapper) {    
+    std::thread::spawn(move || {
+	let mut last_entry: serde_json::Value = serde_json::json!(null);
+	loop {
+	    std::thread::sleep(std::time::Duration::from_millis(1000));
+	    match get_toggl((*handler_wrapper.h).settings.get::<String>("toggl.api_token").unwrap()) {
+		Some(entry) => {
+		    if last_entry == serde_json::json!(null) || entry["data"] != last_entry["data"] {
+			if last_entry["data"]["id"] != entry["data"]["id"] {
+			    (*handler_wrapper.h).stop_current().unwrap();
+			}
+			(*handler_wrapper.h).current = Some(Block {
+			    name: String::from(entry["data"]["description"].as_str().unwrap_or("")),
+			    start: DateTime::parse_from_rfc3339(entry["data"]["start"].as_str().unwrap()).unwrap().with_timezone(&Local),
+			    end: None,
+			    project: match entry["data"]["pid"].as_u64() {
+				Some(e) => {
+				    Some(Project {
+					name: get_project((*handler_wrapper.h).settings.get::<String>("toggl.api_token").unwrap(), e)
+				    })
+				},
+				None => None,
+			    },
+			    tags: entry["data"]["tags"].as_array().unwrap_or(&Vec::new()).iter().map(
+				|x| Tag {name: String::from(x.as_str().unwrap())}).collect(),
+			    id: nanoid!(8, &HASH_ALPHABET)
+			});
+			last_entry = entry["data"].clone();
+		    }
+		},
+		None => (),
+	    }
+	}
+    });
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     Logger::try_with_env_or_str("info").unwrap()
 	.log_to_file(FileSpec::default().basename("memexd").directory("logs"))
 	.start().unwrap();
     let mut handler = Handler::new();
-    handler.settings.merge(File::with_name(&(env::var("HOME").unwrap()+"/.config/memex.toml"))).unwrap();	
+    handler.settings.merge(File::with_name(&(env::var("HOME").unwrap()+"/.config/memex.toml"))).unwrap();    
     unsafe {
-	struct HandlerWrapper {h: *mut Handler};
-	unsafe impl Send for HandlerWrapper {}
-	unsafe impl Sync for HandlerWrapper {}
 	let handler_wrapper = HandlerWrapper {h: &mut handler as *mut Handler};
-	std::thread::spawn(move || {
-	    let mut last_entry: serde_json::Value = serde_json::json!(null);
-	    loop {
-		std::thread::sleep(std::time::Duration::from_millis(1000));
-		match get_toggl((*handler_wrapper.h).settings.get::<String>("toggl.api_token").unwrap()) {
-		    Some(entry) => {				
-			if last_entry == serde_json::json!(null) || entry["data"] != last_entry["data"] {
-			    if last_entry["data"]["id"] != entry["data"]["id"] {								
-				(*handler_wrapper.h).stop_current();
-			    }
-			    (*handler_wrapper.h).current = Some(Block {
-				name: String::from(entry["data"]["description"].as_str().unwrap_or("")),
-				start: DateTime::parse_from_rfc3339(entry["data"]["start"].as_str().unwrap()).unwrap().with_timezone(&Local),
-				end: None,
-				project: match entry["data"]["pid"].as_u64() {
-				    Some(e) => {
-					Some(Project {
-					    name: get_project((*handler_wrapper.h).settings.get::<String>("toggl.api_token").unwrap(), e)
-					})
-				    },
-				    None => None,
-				},
-				tags: entry["data"]["tags"].as_array().unwrap_or(&Vec::new()).iter().map(
-				    |x| Tag {name: String::from(x.as_str().unwrap())}).collect(),
-				id: nanoid!(8, &ALPHABET)
-			    });
-			    last_entry = entry["data"].clone();
-			}
-		    },
-		    None => (),
-		}
-	    }
-	});	
+	spawn_thread(handler_wrapper);	
     }
     let listener = TcpListener::bind("0.0.0.0:5000").unwrap();
     for stream in listener.incoming() {
 	match stream {
 	    Ok(mut stream) => {
 		let mut buf = [0; mem::size_of::<Request>()];
-		let req: Request;
-		if let Err(s) = stream.read(&mut buf) {
-		    error!("Faile to read from stream: {}", s);
-		}
-		unsafe {req = mem::transmute::<[u8; mem::size_of::<Request>()], Request>(buf);}
+		stream.read(&mut buf)?;
+		let req: Request = serde_json::from_str(&String::from_utf8(buf.to_vec())?)?;
 		match req.query {
-		    Query::ADD(e) => handler.handle_add(&stream, e).await,
+		    Query::ADD(e) => handler.handle_add(e).await?,
 		    Query::GET(s) => handler.handle_get(&stream, s),
 		    Query::LOG(r) => handler.handle_log(&stream, r),
 		}
@@ -340,4 +325,5 @@ async fn main() {
 	}
     }
     drop(listener);
+    Ok(())
 }
--- src/toggl.rs
+++ src/toggl.rs
@@ -3,101 +3,101 @@ use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Entry {
-	description: String,
-	tags: Vec<String>,
-	pid: u64,
-	created_with: String,
+    description: String,
+    tags: Vec<String>,
+    pid: u64,
+    created_with: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Post {
-	time_entry: Entry,
+    time_entry: Entry,
 }
 
 pub async fn get_projects(token: String, workspace: u64) -> HashMap<String, u64> {
-	let mut map: HashMap<String, u64> = HashMap::new();
-	let client = reqwest::Client::new();
-	let res = client.get(String::from("https://api.track.toggl.com/api/v9/workspaces/")+&workspace.to_string()+"/projects")
-		.basic_auth(token, Some("api_token"))
-		.send().await.unwrap().text().await.unwrap();
-	let json: Vec<serde_json::Value> = serde_json::from_str(&res).unwrap();
-	for i in json {
-		map.insert(i["name"].as_str().unwrap().to_string(),
-				   i["id"].as_u64().unwrap()); // HACK
-	}
-	return map;
+    let mut map: HashMap<String, u64> = HashMap::new();
+    let client = reqwest::Client::new();
+    let res = client.get(String::from("https://api.track.toggl.com/api/v9/workspaces/")+&workspace.to_string()+"/projects")
+	.basic_auth(token, Some("api_token"))
+	.send().await.unwrap().text().await.unwrap();
+    let json: Vec<serde_json::Value> = serde_json::from_str(&res).unwrap();
+    for i in json {
+	map.insert(i["name"].as_str().unwrap().to_string(),
+		   i["id"].as_u64().unwrap()); // HACK
+    }
+    return map;
 }
 
 pub async fn get_proj_id(token: String, map: HashMap<String, u64>, name: String) -> u64 {
-	if let Some(id) = map.get(&name) {
-		return *id;
-	}
-	let client = reqwest::Client::new();	
-	let res: serde_json::Value = client.post("https://api.track.toggl.com/api/v8/projects")
-		.header("Content-Type", "application/json")
-		.basic_auth(token, Some("api_token"))
-		.body(String::from("{\"project\":{\"name\":\"")+&name+"\", \"wid\": 3816613}}")
-		.send().await.unwrap()
-		.json().await.unwrap();
-	return res["data"]["id"].as_u64().unwrap();
+    if let Some(id) = map.get(&name) {
+	return *id;
+    }
+    let client = reqwest::Client::new();	
+    let res: serde_json::Value = client.post("https://api.track.toggl.com/api/v8/projects")
+	.header("Content-Type", "application/json")
+	.basic_auth(token, Some("api_token"))
+	.body(String::from("{\"project\":{\"name\":\"")+&name+"\", \"wid\": 3816613}}")
+	.send().await.unwrap()
+	.json().await.unwrap();
+    return res["data"]["id"].as_u64().unwrap();
 } 
 
 pub fn get_project(token: String, id: u64) -> String {
-	let client = reqwest::blocking::Client::new();
-	let json: serde_json::Value = client.get(String::from("https://api.track.toggl.com/api/v8/projects/")+&id.to_string())		
-		.basic_auth(token, Some("api_token")).send().unwrap()
-		.json().unwrap();
-	return String::from(json["data"]["name"].as_str().unwrap());
+    let client = reqwest::blocking::Client::new();
+    let json: serde_json::Value = client.get(String::from("https://api.track.toggl.com/api/v8/projects/")+&id.to_string())		
+	.basic_auth(token, Some("api_token")).send().unwrap()
+	.json().unwrap();
+    return String::from(json["data"]["name"].as_str().unwrap());
 }
 
 pub fn get_toggl(token: String) -> Option<serde_json::Value> {
-	let client = reqwest::blocking::Client::new();
-	match client.get("https://api.track.toggl.com/api/v8/time_entries/current")		
-		.basic_auth(token, Some("api_token")).send() {
-		Ok(res) => match res.json() {
-			Ok(json) => return json,
-			Err(_) => return None,
-		}
+    let client = reqwest::blocking::Client::new();
+    match client.get("https://api.track.toggl.com/api/v8/time_entries/current")		
+	.basic_auth(token, Some("api_token")).send() {
+	    Ok(res) => match res.json() {
+		Ok(json) => return json,
 		Err(_) => return None,
+	    }
+	    Err(_) => return None,
 	}
 }
 
 pub async fn set_toggl(token: String, name: String, pid: u64) {
-	let client = reqwest::Client::new();
-	let post = Post {
-		time_entry: Entry {
-			description: name,
-			tags: Vec::new(),
-			pid: pid,
-			created_with: String::from("memex"),
-		}
-	};
-	let res = client.post("https://api.track.toggl.com/api/v8/time_entries/start")
-		.header("Content-Type", "application/json")
-		.basic_auth(token, Some("api_token"))
-		.json(&post)
-		.send().await;
+    let client = reqwest::Client::new();
+    let post = Post {
+	time_entry: Entry {
+	    description: name,
+	    tags: Vec::new(),
+	    pid: pid,
+	    created_with: String::from("memex"),
+	}
+    };
+    client.post("https://api.track.toggl.com/api/v8/time_entries/start")
+	.header("Content-Type", "application/json")
+	.basic_auth(token, Some("api_token"))
+	.json(&post)
+	.send().await.unwrap();
 }
 
 pub async fn update_toggl(token: String, name: String, tags: Vec<String>, pid: u64) {
-	let client = reqwest::Client::new();
-	let res = client.get("https://api.track.toggl.com/api/v8/time_entries/current")		
-		.basic_auth(token.clone(), Some("api_token"))
-		.send().await.unwrap().text().await.unwrap();
-	let cur: serde_json::Value = serde_json::from_str(&res).unwrap();
-	let client = reqwest::Client::new();
-	let post = Post {
-		time_entry: Entry {
-			description: name,
-			tags: tags,
-			pid: pid,
-			created_with: String::from("memex"),
-		}
-	};
- 	let res = client.put(String::from("https://api.track.toggl.com/api/v8/time_entries/")+&cur["data"]["id"].to_string())
-		.header("Content-Type", "application/json")
-		.basic_auth(token.clone(), Some("api_token"))
-		.json(&post)
-		.send().await;
+    let client = reqwest::Client::new();
+    let res = client.get("https://api.track.toggl.com/api/v8/time_entries/current")		
+	.basic_auth(token.clone(), Some("api_token"))
+	.send().await.unwrap().text().await.unwrap();
+    let cur: serde_json::Value = serde_json::from_str(&res).unwrap();
+    let client = reqwest::Client::new();
+    let post = Post {
+	time_entry: Entry {
+	    description: name,
+	    tags: tags,
+	    pid: pid,
+	    created_with: String::from("memex"),
+	}
+    };
+    client.put(String::from("https://api.track.toggl.com/api/v8/time_entries/")+&cur["data"]["id"].to_string())
+	.header("Content-Type", "application/json")
+	.basic_auth(token.clone(), Some("api_token"))
+	.json(&post)
+	.send().await.unwrap();
 }
 
